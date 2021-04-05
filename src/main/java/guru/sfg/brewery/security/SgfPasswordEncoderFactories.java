cac2ad28bf97fddfe3bb1af513ffe10f0c4e2352
roman.gritsko <roman.gritsko@russianpost.ru>
2021-04-05 13:55:35 +0300

    security config - use custom DelegatingPasswordEncoder

---
+++ src/main/java/guru/sfg/brewery/security/SgfPasswordEncoderFactories.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2020 Russian Post
+ *
+ * This source code is Russian Post Confidential Proprietary.
+ * This software is protected by copyright. All rights and titles are reserved.
+ * You shall not use, copy, distribute, modify, decompile, disassemble or reverse engineer the software.
+ * Otherwise this violation would be treated by law and would be subject to legal prosecution.
+ * Legal use of the software provides receipt of a license from the right holder only.
+ */
+
+package guru.sfg.brewery.security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SgfPasswordEncoderFactories {
+
+    public static PasswordEncoder createDelegatingPasswordEncoder() {
+        String encodingId = "bcrypt";
+        Map<String, PasswordEncoder> encoders = new HashMap();
+        encoders.put(encodingId, new BCryptPasswordEncoder());
+        encoders.put("ldap", new LdapShaPasswordEncoder());
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+        encoders.put("sha256", new StandardPasswordEncoder());
+
+        return new DelegatingPasswordEncoder(encodingId, encoders);
+    }
+
+    private SgfPasswordEncoderFactories() {
+    }
+}
