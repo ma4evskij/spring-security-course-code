9cfd1a4dd45281dd35224ba7096dd9ed6af6360c
roman.gritsko <roman.gritsko@russianpost.ru>
2021-04-13 12:19:03 +0300

    ---

--- src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
+++ src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
@@ -12,6 +12,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -34,15 +36,33 @@ public class UserDataLoader implements CommandLineRunner {
         Authority updateBeer = authorityRepository.save(Authority.builder().permission("beer.update").build());
         Authority deleteBeer = authorityRepository.save(Authority.builder().permission("beer.delete").build());
 
+        Authority createCustomer = authorityRepository.save(Authority.builder().permission("customer.create").build());
+        Authority readCustomer = authorityRepository.save(Authority.builder().permission("customer.read").build());
+        Authority updateCustomer = authorityRepository.save(Authority.builder().permission("customer.update").build());
+        Authority deleteCustomer = authorityRepository.save(Authority.builder().permission("customer.delete").build());
+
+        Authority createBrewery = authorityRepository.save(Authority.builder().permission("brewery.create").build());
+        Authority readBrewery = authorityRepository.save(Authority.builder().permission("brewery.read").build());
+        Authority updateBrewery = authorityRepository.save(Authority.builder().permission("brewery.update").build());
+        Authority deleteBrewery = authorityRepository.save(Authority.builder().permission("brewery.delete").build());
+
         Role adminRole = roleRepository.save(Role.builder().name("ADMIN").build());
         Role userRole = roleRepository.save(Role.builder().name("USER").build());
         Role customerRole = roleRepository.save(Role.builder().name("CUSTOMER").build());
 
-        adminRole.setAuthorities(Set.of(createBeer, readBeer, updateBeer, deleteBeer));
-        userRole.setAuthorities(Set.of(readBeer));
-        customerRole.setAuthorities(Set.of(readBeer));
+        adminRole.setAuthorities(
+                new HashSet<>(Set.of(
+                        createBeer, readBeer, updateBeer, deleteBeer,
+                        createCustomer, readCustomer, updateCustomer, deleteCustomer,
+                        createBrewery, readBrewery, updateBrewery, deleteBrewery
+                ))
+        );
 
-        roleRepository.saveAll(Set.of(adminRole, customerRole, userRole));
+        userRole.setAuthorities(new HashSet<>(Set.of(readBeer)));
+
+        customerRole.setAuthorities(new HashSet<>(Set.of(readBeer, readCustomer, readBrewery)));
+
+        roleRepository.saveAll(Arrays.asList(adminRole, customerRole, userRole));
 
         userRepository.save(User.builder()
                 .username("spring")
--- src/main/java/guru/sfg/brewery/domain/security/Role.java
+++ src/main/java/guru/sfg/brewery/domain/security/Role.java
@@ -48,7 +48,7 @@ public class Role {
     @Singular
     @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST}, fetch = FetchType.EAGER)
     @JoinTable(name = "role_authority",
-            joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
-            inverseJoinColumns = {@JoinColumn(name = "ROLE_ID", referencedColumnName = "ID")})
+            joinColumns = {@JoinColumn(name = "ROLE_ID", referencedColumnName = "ID")},
+            inverseJoinColumns = {@JoinColumn(name = "AUTHORITY_ID", referencedColumnName = "ID")})
     private Set<Authority> authorities;
 }
